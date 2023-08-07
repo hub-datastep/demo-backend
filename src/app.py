fe3ce6d02e77080364b827ce73179289080b6d57
Dmitry Bleschunov <bleschunov.dmitry@gmail.com>
2023-08-07 17:29:32 +0300

    add cors

--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
 import uvicorn as uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from controller import datastep_controller
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(datastep_controller.router)
 
