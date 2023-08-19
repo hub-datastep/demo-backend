032ad18fc932c2e47532af6a59189cfc7d5deaf6
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-08-19 14:50:39 +0300

    fix tables and fly.tomls

--- src/service/datastep_service.py
+++ src/service/datastep_service.py
@@ -2,7 +2,7 @@ from datastep.components.chain import get_db, get_llm
 from datastep.components.sql_database_chain_executor import get_sql_database_chain_executor
 
 datastep_service = get_sql_database_chain_executor(
-    get_db(),
+    get_db(tables=["test"]),
     get_llm(model_name="gpt-3.5-turbo-16k"),
     debug=False,
     return_intermediate_steps=True
