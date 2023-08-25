85a995c85336580280cf6b671030b1bc986dc25c
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-08-25 19:12:14 +0300

    update gitignore and change view name

--- src/service/datastep_service.py
+++ src/service/datastep_service.py
@@ -2,7 +2,7 @@ from datastep.components.chain import get_db, get_llm
 from datastep.components.sql_database_chain_executor import get_sql_database_chain_executor
 
 datastep_service = get_sql_database_chain_executor(
-    get_db(tables=["УСО БДДС - С начала 2022 года - ПЗЕ - 24.08.2023"]),
+    get_db(tables=["УСО БДДС - С начала 2022 года - ПЗЕ"]),
     get_llm(model_name="gpt-3.5-turbo-16k"),
     use_memory=False,
     debug=False,
