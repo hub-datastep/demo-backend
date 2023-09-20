64757c4e016ba4d79ad3e855177104c8727bc420
Syrnnik <sakh.alexandr@gmail.com>
2023-09-20 23:20:57 +0300

    feat: update chat dto with source prop

--- src/dto/chat_dto.py
+++ src/dto/chat_dto.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pydantic import BaseModel
 
 from dto.message_dto import MessageOutDto
+from dto.source_dto import SourceOutDto
 
 
 class ChatCreateDto(BaseModel):
@@ -13,3 +14,4 @@ class ChatOutDto(ChatCreateDto):
     id: int
     created_at: datetime
     message: list[MessageOutDto] | None = None
+    source: list[SourceOutDto]
