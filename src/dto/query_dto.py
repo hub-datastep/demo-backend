d2fe4f307dead3457c30542016249255661c0cbb
Syrnnik <sakh.alexandr@gmail.com>
2023-09-21 01:40:11 +0300

    fix: source id prop in query dto

--- src/dto/query_dto.py
+++ src/dto/query_dto.py
@@ -1,8 +1,7 @@
-from fastapi import UploadFile
 from pydantic import BaseModel
 
 
 class QueryDto(BaseModel):
     chat_id: int | None = None
     query: str
-    source_id: str
+    source_id: str | None = None
