8be72075d4cc6c6cd76b398a324c7937144f8ef5
Syrnnik <sakh.alexandr@gmail.com>
2023-09-21 17:54:24 +0300

    fix: upload file & save source in single request

--- src/controller/source_controller.py
+++ src/controller/source_controller.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile
 from fastapi_versioning import version
 
-from dto.source_dto import SourceCreateDto
 from dto.user_dto import UserDto
+from model import source_model
 from repository.source_repository import source_repository
 from service.auth_service import AuthService
 
@@ -24,7 +24,7 @@ async def get_last_source(chat_id: int, current_user: UserDto = Depends(AuthServ
     return source_repository.get_last_source(chat_id)
 
 
-@router.post("")
+@router.post("/{chat_id}")
 @version(1)
-async def save_source(body: SourceCreateDto, current_user: UserDto = Depends(AuthService.get_current_user)):
-    return source_repository.save_source(body)
+async def upload_source(chat_id: int, fileObject: UploadFile, current_user: UserDto = Depends(AuthService.get_current_user)):
+    return source_model.save_source(chat_id, fileObject)
--- src/repository/source_repository.py
+++ src/repository/source_repository.py
@@ -1,40 +1,39 @@
-from dto.source_dto import SourceCreateDto, SourceOutDto
+from dto.source_dto import SourceCreateDto, SourceDto
 from infra.supabase import supabase
 
 
 class SourceRepository:
     @classmethod
-    def get_last_source(cls, chat_id: int) -> SourceOutDto:
+    def get_last_source(cls, chat_id: int) -> SourceDto:
         (_, sources), _ = supabase\
             .table("source")\
             .select("*")\
             .eq("chat_id", chat_id)\
-            .order("id")\
+            .order("created_at", desc=True)\
             .limit(1)\
             .execute()
 
         if len(sources) == 0:
             return None
 
-        return SourceOutDto(**(sources[0]))
+        return SourceDto(**(sources[0]))
 
     @classmethod
-    def get_all_sources(cls, chat_id: int) -> list[SourceOutDto]:
+    def get_all_sources(cls, chat_id: int) -> list[SourceDto]:
         (_, sourcesList), _ = supabase\
             .table("source")\
             .select("*")\
             .eq("chat_id", chat_id)\
             .execute()
-        return [SourceOutDto(**sourceItem) for sourceItem in sourcesList]
+        return [SourceDto(**sourceItem) for sourceItem in sourcesList]
 
     @classmethod
-    def save_source(cls, body: SourceCreateDto) -> SourceOutDto:
+    def save_source(cls, body: SourceCreateDto) -> SourceDto:
         (_, [source]), _ = supabase\
             .table("source")\
             .insert(body.model_dump())\
             .execute()
-        return SourceOutDto(**source)
+        return SourceDto(**source)
 
 
-source_repository = SourceRepository()
 source_repository = SourceRepository()
