dba24225f9d0ced2613c368b9f28416bfd0faca4
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-09-25 16:12:33 +0600

    set GTP-4 and change timestamp template in prompt

--- src/service/datastep_service.py
+++ src/service/datastep_service.py
@@ -4,7 +4,7 @@ from datastep.components.sql_database_chain_executor import get_sql_database_cha
 
 datastep_service = get_sql_database_chain_executor(
     get_db(),
-    get_llm(model_name="gpt-3.5-turbo-16k"),
+    get_llm(model_name="gpt-4"),
     debug=True,
     verbose_answer=config["verbose_answer"]
 )
