637812348c4d581f5926862548b0dcafe9d3a7e2
Syrnnik <sakh.alexandr@gmail.com>
2023-09-30 18:02:19 +0300

    fix: questions endpoint to POST

--- src/controller/question_controller.py
+++ src/controller/question_controller.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from fastapi_versioning import version
 
+from dto.question_dto import QuestionGetDto
 from dto.user_dto import UserDto
 from model import question_model
 from service.auth_service import AuthService
@@ -11,7 +12,7 @@ router = APIRouter(
 )
 
 
-@router.get("/{limit}")
+@router.post("/")
 @version(1)
-async def get_template_questions(limit: int, current_user: UserDto = Depends(AuthService.get_current_user)):
-    return question_model.get_random_questions(limit)
+async def get_template_questions(body: QuestionGetDto, current_user: UserDto = Depends(AuthService.get_current_user)):
+    return question_model.get_random_questions(body.tables, body.limit)
--- src/dto/question_dto.py
+++ src/dto/question_dto.py
@@ -2,5 +2,10 @@ from pydantic import BaseModel
 
 
 class QuestionDto(BaseModel):
-    id: int
+    id: int | None = None
     question: str
+
+
+class QuestionGetDto(BaseModel):
+    tables: list[str]
+    limit: int = 3
