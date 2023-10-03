5fadae6cb0d2c4328b1a2341f99b23a71b756d47
Syrnnik <sakh.alexandr@gmail.com>
2023-10-04 02:18:26 +0300

    feat: get active prompt by tenant id

--- src/repository/prompt_repository.py
+++ src/repository/prompt_repository.py
@@ -1,5 +1,5 @@
 from config.config import config
-from dto.prompt_out_dto import PromptOutDto, PromptEditDto
+from dto.prompt_out_dto import PromptEditDto, PromptOutDto
 from infra.supabase import supabase
 
 
@@ -7,6 +7,20 @@ class PromptRepository:
     @classmethod
     def fetch_by_id(cls, prompt_id: int) -> PromptOutDto:
         (_, [prompt]), _ = supabase.table("prompt").select("*").eq("id", prompt_id).execute()
+        return PromptOutDto(**prompt)
+
+    @classmethod
+    def get_active_prompt_by_tenant_id(cls, tenant_id: int) -> PromptOutDto:
+        (_, [prompt]), _ = supabase\
+            .table("prompt")\
+            .select("*")\
+            .eq("tenant_id", tenant_id)\
+            .eq("is_active", True)\
+            .execute()
+        # TODO: при первом создании тенанта, промта явно не будет, если его не будет, то будет вылетать ошибка, надо будет это предусмотреть
+        # TODO case 1: сделать для промта дефолтное значение - пустую строку (супабейз такое умеет)
+        # TODO case 2: сделать дефолтное значение для промтов в супабейзе
+        # TODO case 3: выдавать exception что промта нет
         return PromptOutDto(**prompt)
 
     @classmethod
