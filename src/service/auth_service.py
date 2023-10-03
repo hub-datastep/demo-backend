bd39120d001f414b7ed774de4e8441c4b73501b6
Syrnnik <sakh.alexandr@gmail.com>
2023-10-04 02:20:45 +0300

    feat: add tenant id to current user in AuthService

--- src/service/auth_service.py
+++ src/service/auth_service.py
@@ -6,6 +6,7 @@ from gotrue.errors import AuthApiError
 from dto.auth_dto import AuthDto
 from dto.user_dto import UserDto
 from infra.supabase import supabase
+from repository.user_tenant_repository import user_tenant_repository
 
 
 class AuthService:
@@ -14,7 +15,10 @@ class AuthService:
     @classmethod
     def sign_in(cls, username: str, password: str) -> AuthDto:
         try:
-            (_, (_, session)) = supabase.auth.sign_in_with_password({"email": username, "password": password})
+            (_, (_, session)) = supabase.auth.sign_in_with_password({
+                "email": username,
+                "password": password
+            })
             return AuthDto(
                 access_token=session.access_token,
                 refresh_token=session.refresh_token
@@ -27,6 +31,12 @@ class AuthService:
         # TODO: получить айди тенанта из базы или кинуть ошибку, если такого тенанта нет и положить его в UserDto
         try:
             user_response: UserResponse = supabase.auth.get_user(token)
-            return UserDto(email=user_response.user.email)
+            user_id = user_response.user.id
+            tenant_id = user_tenant_repository.get_tenant_id_by_user_id(user_id)
+
+            return UserDto(
+                email=user_response.user.email,
+                tenant_id=tenant_id
+            )
         except AuthApiError as e:
             raise HTTPException(status_code=e.status, detail=e.message)
