eb945cb0bf316852fd8dd0612e0a78b449ed678a
Syrnnik <sakh.alexandr@gmail.com>
2023-10-04 02:21:30 +0300

    feat: get db uri & active prompt from supabase

--- src/model/datastep_model.py
+++ src/model/datastep_model.py
@@ -1,21 +1,26 @@
 import pandas as pd
 
 from datastep.components.datastep_sql_database import DatastepSqlDatabase
-from dto.datastep_prediction_dto import DatastepPredictionDto
 from datastep.datastep_chains.datastep_markdown_chain import DatastepMarkdownChain
 from datastep.datastep_chains.datastep_sql_chain import DatastepSqlChain
+from dto.datastep_prediction_dto import DatastepPredictionDto
 from dto.query_dto import QueryDto
+from repository.prompt_repository import prompt_repository
+from repository.tenant_repository import tenant_repository
 
 
-async def datastep_get_prediction(body: QueryDto) -> DatastepPredictionDto:
-    # TODO: Брать connection string из файлика
-    # TODO: PromptRepository.get_active_prompt(tenant_id); tenant_id брать из аргументов функции
+async def datastep_get_prediction(body: QueryDto, tenant_id: int) -> DatastepPredictionDto:
+    tenant_db_uri = tenant_repository.get_db_uri_by_id(tenant_id)
+    tenant_active_prompt_template = prompt_repository.get_active_prompt_by_tenant_id(tenant_id)
 
     datastep_sql_database = DatastepSqlDatabase(
-        database_connection_string="postgresql://qvzibsah:7wNHUBB3BMnY6QiFi0ggBejNd3SmlRf2@trumpet.db.elephantsql.com/qvzibsah",
+        database_connection_string=tenant_db_uri,
         include_tables=body.tables
     )
-    datastep_sql_chain = DatastepSqlChain(sql_database=datastep_sql_database.database)
+    datastep_sql_chain = DatastepSqlChain(
+        sql_database=datastep_sql_database.database,
+        prompt_template=tenant_active_prompt_template
+    )
     # TODO: Можно ли без него обойтись
     datastep_markdown_chain = DatastepMarkdownChain()
 
