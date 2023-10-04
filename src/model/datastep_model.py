f9afdf332cbf6fb7f6b0cbafb2b6a8459b0cc982
Syrnnik <sakh.alexandr@gmail.com>
2023-10-04 21:18:47 +0300

    refactor: tenant repo method name

--- src/model/datastep_model.py
+++ src/model/datastep_model.py
@@ -10,7 +10,7 @@ from repository.tenant_repository import tenant_repository
 
 
 async def datastep_get_prediction(body: QueryDto, tenant_id: int) -> DatastepPredictionDto:
-    tenant_db_uri = tenant_repository.get_db_uri_by_id(tenant_id)
+    tenant_db_uri = tenant_repository.get_db_uri_by_tenant_id(tenant_id)
     tenant_active_prompt_template = prompt_repository.get_active_prompt_by_tenant_id(tenant_id)
 
     datastep_sql_database = DatastepSqlDatabase(
