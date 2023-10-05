e08980bb564503e1fff44e7b2212e073ac29ba69
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-10-05 17:23:04 +0600

    new chain architecture done; it works

--- src/datastep/components/datastep_sql_database.py
+++ src/datastep/components/datastep_sql_database.py
@@ -7,17 +7,19 @@ class DatastepSqlDatabase:
     def __init__(
         self,
         database_connection_string: str,
-        include_tables: list[str]
+        include_tables: list[str],
+        tenant_id: int
     ):
+        print(database_connection_string)
         # TODO: Использовать пулинг из Алхимии
-        if not databases_connection_pool.get("foo", None):
-            databases_connection_pool["foo"] = SQLDatabase.from_uri(
+        if not databases_connection_pool.get(tenant_id, None):
+            databases_connection_pool[tenant_id] = SQLDatabase.from_uri(
                 database_connection_string,
                 include_tables=include_tables,
                 view_support=True
             )
 
-        self.database = databases_connection_pool["foo"]
+        self.database = databases_connection_pool[tenant_id]
 
     def run(self, sql_query):
         return self.database._execute(sql_query)
