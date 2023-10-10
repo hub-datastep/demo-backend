27ac45e0289c2615ade3bc1158af2b7c69734e7b
Syrnnik <sakh.alexandr@gmail.com>
2023-10-10 19:32:23 +0300

    fix: return source model

--- src/model/source_model.py
+++ src/model/source_model.py
@@ -3,21 +3,18 @@ from fastapi import UploadFile
 from dto.source_dto import SourceCreateDto, SourceDto
 from repository.source_repository import source_repository
 from service.chat_pdf_service import ChatPdfService
-from service.supastorage_service import upload_file_to_supastorage
 
 
 def save_source(chat_id: int, fileObject: UploadFile) -> SourceDto:
-    # source_service = ChatPdfService
+    source_service = ChatPdfService
 
-    source = upload_file_to_supastorage(fileObject)
-
-    # source_id = source_service.upload_file(fileObject.file)
-    # source = source_repository.save_source(
-    #     SourceCreateDto(
-    #         source_id=source_id,
-    #         chat_id=chat_id,
-    #         file_name=fileObject.filename,
-    #     )
-    # )
+    source_id = source_service.upload_file(fileObject.file)
+    source = source_repository.save_source(
+        SourceCreateDto(
+            source_id=source_id,
+            chat_id=chat_id,
+            file_name=fileObject.filename,
+        )
+    )
 
     return source
