618c35482d699b628d602a32087d592d7efd2316
Syrnnik <sakh.alexandr@gmail.com>
2023-10-10 19:32:41 +0300

    feat: save file to files table in db

--- src/repository/file_repository.py
+++ src/repository/file_repository.py
@@ -5,3 +5,11 @@ from infra.supabase import supabase
 def get_all_filenames_ru() -> list[FileDto]:
     (_, filenames), _ = supabase.table("file").select("*").execute()
     return [FileDto(**filename) for filename in filenames]
+
+
+def save_file(body: FileDto) -> FileDto:
+    (_, [file]), _ = supabase\
+        .table("file")\
+        .insert(body.model_dump())\
+        .execute()
+    return FileDto(**file)
