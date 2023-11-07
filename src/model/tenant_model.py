e0f65a307dc0fe73e72d7bdd397b3f48d921072c
Kate Kovtun <katerina.kvtn.skh@gmail.com>
2023-11-07 20:48:04 +0300

    fix: update create_tenant_with_user_id

--- src/model/tenant_model.py
+++ src/model/tenant_model.py
@@ -1,17 +1,16 @@
 from repository.tenant_repository import tenant_repository
 from constant.logo_default_path import LOGO_DEFAULT_PATH
 from constant.db_uri_default_path import DB_URI_DEFAULT_PATH
-from dto.tenant_dto import TenantCreateDto, TenantDto
+from dto.tenant_dto import TenantCreateDto
+from service.auth_service import AuthService
 
-
-def create_tenant_with_user_id(user_id: str):
-
+def create_tenant_with_user_id(user_id: str, email: str):
     tenant_id = tenant_repository.get_tenant_id_by_user_id(user_id)
-
+    
     if tenant_id == None:
         new_tenant = tenant_repository.create_tenant(
             TenantCreateDto(
-                name="new tenant",
+                name= email.split("@")[0],
                 logo=LOGO_DEFAULT_PATH,
                 db_uri=DB_URI_DEFAULT_PATH,
             )
