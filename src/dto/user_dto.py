8fa30bf245e51d58cedb9769e04370f2c68b8fb5
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-11-21 18:50:41 +0600

    start

--- src/dto/user_dto.py
+++ src/dto/user_dto.py
@@ -3,9 +3,14 @@ from pydantic import BaseModel
 from dto.config_dto import DatabasePredictionConfigDto
 
 
-class UserDto(BaseModel):
+class UserOutDto(BaseModel):
     id: str
     email: str
     tenant_id: int
     available_modes: list[str]
     database_prediction_config: DatabasePredictionConfigDto | None
+
+
+class UserCreateDto(UserOutDto):
+    password: str
+
---
+++ src/model/auth_model.py
@@ -0,0 +1,23 @@
+from dto.user_dto import UserOutDto
+
+def get_jwt():
+    pass
+
+
+def validate_jwt():
+    pass
+
+
+def get_hashed_password():
+    pass
+
+
+def sign_up(username: str, password: str) -> UserOutDto:
+
+
+
+def sign_in():
+    pass
+
+
+
