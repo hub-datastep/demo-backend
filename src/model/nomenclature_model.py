2eefaf835096d4108d2eaff698bef93e584adea0
Syrnnik <sakh.alexandr@gmail.com>
2024-02-19 19:36:50 +0300

    feat: sort mapped nomenclatures

--- src/model/nomenclature_model.py
+++ src/model/nomenclature_model.py
@@ -5,16 +5,16 @@ import os
 import joblib
 import numpy as np
 import pandas as pd
+from chromadb import HttpClient
 from fastembed.embedding import FlagEmbedding
 from redis import Redis
 from rq import get_current_job
 from rq.job import Job, JobStatus
 from rq.queue import Queue
 from tqdm import tqdm
-from chromadb import HttpClient
 
 from scheme.nomenclature_scheme import NomenclaturesUpload, OneNomenclatureRead, OneNomenclatureUpload, \
-    NomenclaturesRead, JobIdRead
+    NomenclaturesRead, JobIdRead, MappedNomenclature
 
 tqdm.pandas()
 np.set_printoptions(threshold=np.inf)
@@ -26,17 +26,28 @@ def map_on_group(noms: pd.DataFrame) -> list:
     return model.predict(count_vect.transform(noms["nomenclature"]))
 
 
-def map_on_nom(nom_embeddings: np.ndarray,  group: str):
+def map_on_nom(nom_embeddings: np.ndarray, group: str, most_similar_count: int):
     chroma = HttpClient(host=os.getenv("CHROMA_HOST"), port=os.getenv("CHROMA_PORT"))
     collection = chroma.get_collection(name="nomenclature")
 
     nom_embeddings = nom_embeddings.tolist()
     response = collection.query(
         query_embeddings=[nom_embeddings],
-        n_results=1,
+        n_results=most_similar_count,
         where={"group": group}
     )
-    return response["documents"][0]
+
+    mapped_noms = []
+    for i in range(most_similar_count):
+        mapped_noms.append(MappedNomenclature(
+            nomenclature_guid=response["ids"][i],
+            nomenclature=response["documents"][i],
+            similarity_score=response["distances"][i],
+        ))
+    # Sort from most similar to lower
+    mapped_noms.sort(key=lambda nom: nom.similarity_score, reverse=True)
+
+    return mapped_noms
 
 
 def parse_txt_file(nomenclatures: list[OneNomenclatureUpload]) -> pd.DataFrame:
@@ -79,36 +90,45 @@ def nomenclature_segments(
         yield nomenclatures[i:i + segment_length]
 
 
-def create_job(nomenclatures: list[OneNomenclatureUpload], previous_job_id: str | None) -> JobIdRead:
+def create_job(
+    nomenclatures: list[OneNomenclatureUpload],
+    previous_job_id: str | None,
+    most_similar_count: int
+) -> JobIdRead:
     redis = Redis(host=os.getenv("REDIS_HOST"), password=os.getenv("REDIS_PASSWORD"))
     queue = Queue(name="nomenclature", connection=redis)
     job = queue.enqueue(
         process,
-        nomenclatures,
+        {
+            "nomenclatures": nomenclatures,
+            "most_similar_count": most_similar_count,
+        },
         meta={
             "previous_nomenclature_id": previous_job_id
         },
         result_ttl=-1,
         job_timeout=3600 * 24
     )
-    return JobIdRead(nomenclature_id=job.id)
+    return JobIdRead(job_id=job.id)
 
 
 def start_mapping(nomenclatures: NomenclaturesUpload) -> JobIdRead:
     nomenclatures_list = nomenclatures.nomenclatures
+    most_similar_count = nomenclatures.most_similar_count
 
-    last_nomenclature_id = None
+    last_job_id = None
     for segment in nomenclature_segments(nomenclatures_list, segment_length=nomenclatures.job_size):
         job = create_job(
             nomenclatures=segment,
-            previous_job_id=last_nomenclature_id
+            previous_job_id=last_job_id,
+            most_similar_count=most_similar_count
         )
-        last_nomenclature_id = job.nomenclature_id
+        last_job_id = job.job_id
 
-    return JobIdRead(nomenclature_id=last_nomenclature_id)
+    return JobIdRead(job_id=last_job_id)
 
 
-def process(nomenclatures: list[OneNomenclatureUpload]):
+def process(nomenclatures: list[OneNomenclatureUpload], most_similar_count: int):
     job = get_current_job()
 
     noms: pd.DataFrame = parse_txt_file(nomenclatures)
@@ -125,7 +145,7 @@ def process(nomenclatures: list[OneNomenclatureUpload]):
 
     with tqdm(total=len(noms)) as pbar:
         for i, nom in noms.iterrows():
-            nom.mapping = map_on_nom(nom.embeddings, nom.group)
+            nom.mapping = map_on_nom(nom.embeddings, nom.group, most_similar_count)
             noms.loc[i] = nom
             job.meta["ready_count"] += 1
             job.save_meta()
@@ -146,7 +166,7 @@ def get_jobs_from_rq(nomenclature_id: str) -> list[NomenclaturesRead]:
         job_meta = job.get_meta(refresh=True)
         job_status = job.get_status(refresh=True)
         job_result = NomenclaturesRead(
-            nomenclature_id=job.id,
+            job_id=job.id,
             ready_count=job_meta.get("ready_count", None),
             total_count=job_meta.get("total_count", None),
             general_status=job_status,
@@ -170,8 +190,8 @@ def get_all_jobs(nomenclature_id: str) -> list[NomenclaturesRead]:
 
 
 if __name__ == "__main__":
-    import pprint
     from dotenv import load_dotenv
+
     load_dotenv()
     # from dotenv import load_dotenv
     #
