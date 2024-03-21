addebb05443be4f611b3a017afff56b7a862afeb
Syrnnik <sakh.alexandr@gmail.com>
2024-03-21 23:08:44 +0300

    fix: sync loggic

--- src/model/synchronize_embeddings_model.py
+++ src/model/synchronize_embeddings_model.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta
 
 from pandas import DataFrame, read_sql
 
-from infra.chroma_store import delete_embeddings, add_embeddings, update_embeddings, is_in_vectorstore, connect_to_chroma_collection
+from infra.chroma_store import delete_embeddings, add_embeddings, update_embeddings, is_in_vectorstore, \
+    connect_to_chroma_collection
 
 
 def fetch_nomenclatures(nom_db_con_str: str, table_name: str, sync_period: int) -> DataFrame:
@@ -18,18 +19,19 @@ def fetch_nomenclatures(nom_db_con_str: str, table_name: str, sync_period: int) 
     return read_sql(st, nom_db_con_str)
 
 
-def get_root_group(nom_db_con_str: str, table_name: str, parent) -> DataFrame:
+def get_root_group_name(nom_db_con_str: str, table_name: str, parent):
     current_parent = parent
-    root_group_name = ""
-    while current_parent != 'e29a2c8c-dad2-11ea-8e35-001e67bc1b0d':
+    root_group = DataFrame()
+    while current_parent != "00000000-0000-0000-0000-000000000000":
         st = f"""
             SELECT *
             FROM {table_name}
             WHERE "Ссылка" = '{current_parent}'
         """
         root_group = read_sql(st, nom_db_con_str)
-        root_group_name = root_group['Наименование'].item()
-        current_parent = root_group['Родитель'].item()
+        current_parent = str(root_group['Родитель'].item())
+
+    root_group_name = root_group['Наименование'].item()
     return root_group_name
 
 
@@ -42,30 +44,29 @@ def synchronize_nomenclatures(
     nomenclatures = fetch_nomenclatures(nom_db_con_str, table_name, sync_period)
     collection = connect_to_chroma_collection(chroma_collection_name)
     for _, row in nomenclatures.iterrows():
-        guid = row['Ссылка']
-        parent = row['Родитель']
-        name = row['Наименование']
+        guid = str(row['Ссылка'])
+        parent = str(row['Родитель'])
+        name = str(row['Наименование'])
         metadatas = {"group": parent}
+        root_group_name = get_root_group_name(nom_db_con_str, table_name, parent)
 
-        if is_in_vectorstore(guid):
-            add_embeddings(
+        if not is_in_vectorstore(collection=collection, ids=guid):
+            if root_group_name == "0001 Новая структура справочника" and row['ПометкаУдаления'] == 0:
+                add_embeddings(
+                    collection=collection,
+                    ids=guid,
+                    documents=name,
+                    metadatas=metadatas,
+                )
+                print(f"Добавлен: {guid}")
+            continue
+
+        if root_group_name != "0001 Новая структура справочника" or row['ПометкаУдаления'] == 1:
+            delete_embeddings(
                 collection=collection,
                 ids=guid,
-                documents=name,
-                metadatas=metadatas
             )
-            print(f"Добавлен: {guid}")
-            continue
-
-        if row['ПометкаУдаления'] == 1:
-            delete_embeddings(guid)
             print(f"Удалён: {guid}")
-            continue
-
-        root_group = get_root_group(nom_db_con_str, table_name, parent)
-        if root_group != "0001 Новая структура справочника":
-            delete_embeddings(guid)
-            print(f"Корневой родитель {guid} не 0001 Новая структура справочника")
             continue
 
         update_embeddings(
@@ -75,4 +76,3 @@ def synchronize_nomenclatures(
             metadatas=metadatas
         )
         print(f"Обновлён: {guid}")
-
