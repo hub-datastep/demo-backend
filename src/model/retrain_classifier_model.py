32d8d7506e66b026d8f22aac11d2f3b53d3fe29c
Syrnnik <sakh.alexandr@gmail.com>
2024-03-27 11:13:10 +0300

    feat: add rq-worker for retrain classifier

--- src/model/retrain_classifier_model.py
+++ src/model/retrain_classifier_model.py
@@ -1,5 +1,6 @@
+import os
 import re
-from datetime import datetime
+from uuid import uuid4
 
 import joblib
 from pandas import DataFrame, read_sql
@@ -7,7 +8,13 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
+from sqlalchemy import create_engine, text
+from sqlmodel import Session
 from tqdm import tqdm
+
+from infra.redis_queue import get_redis_queue, MAX_JOB_TIMEOUT, get_job
+from scheme.classifier_scheme import ClassifierVersion, ClassifierVersionRead, ClassifierRetrainingResult
+from scheme.nomenclature_scheme import JobIdRead
 
 tqdm.pandas()
 
@@ -16,17 +23,17 @@ _TRAINING_FILE_NAME = "training_data.csv"
 
 
 def _fetch_noms(db_con_str: str, table_name: str) -> DataFrame:
-    st = f"""
+    st = text(f"""
         SELECT Наименование, Родитель
         FROM {table_name}
         WHERE ЭтоГруппа = 0
-    """
+    """)
 
     return read_sql(st, db_con_str)
 
 
 def _fetch_groups(db_con_str: str, table_name: str) -> DataFrame:
-    st = f"""
+    st = text(f"""
         SELECT DISTINCT Наименование, Ссылка
         FROM {table_name}
         WHERE ЭтоГруппа = 1
@@ -34,7 +41,7 @@ def _fetch_groups(db_con_str: str, table_name: str) -> DataFrame:
             Наименование LIKE '__.__.__. %'
             OR Наименование LIKE '__.__. %'
         )
-    """
+    """)
 
     return read_sql(st, db_con_str)
 
@@ -42,12 +49,12 @@ def _fetch_groups(db_con_str: str, table_name: str) -> DataFrame:
 def _has_child(db_con_str: str, table_name: str, group_name: str):
     group_numbs = group_name.split('. ')[0]
 
-    st = f"""
+    st = text(f"""
         SELECT *
         FROM {table_name}
         WHERE Наименование LIKE '{group_numbs}.__. %'
         AND ЭтоГруппа = 1
-    """
+    """)
     children = read_sql(st, db_con_str)
 
     return not children.empty
@@ -90,12 +97,14 @@ def _normalize_nom_name(text: str):
 
 def _get_narrow_group_noms(all_noms: DataFrame, no_child_groups: DataFrame) -> DataFrame:
     # Return noms which Родитель in Ссылка of groups with no child
+    print(f"res {no_child_groups}")
+    print(f"ser {no_child_groups['Ссылка']}")
     narrow_group_noms = all_noms[all_noms['Родитель'].isin(no_child_groups['Ссылка'])]
 
-    return DataFrame(narrow_group_noms)
+    return narrow_group_noms
 
 
-def get_training_data(db_con_str: str, table_name: str) -> DataFrame:
+def _get_training_data(db_con_str: str, table_name: str) -> DataFrame:
     print("Fetching all noms...")
     all_noms = _fetch_noms(db_con_str, table_name)
     print(f"Count of noms: {len(all_noms)}")
@@ -122,15 +131,41 @@ def get_training_data(db_con_str: str, table_name: str) -> DataFrame:
     return narrow_group_noms
 
 
-def get_model_accuracy(classifier, vectorizer: CountVectorizer, x_test, y_test):
+def _get_model_accuracy(classifier, vectorizer: CountVectorizer, x_test, y_test):
     y_pred = classifier.predict(vectorizer.transform(x_test))
     accuracy = accuracy_score(y_test, y_pred)
     return accuracy
 
 
-def retrain_classifier(db_con_str: str, table_name: str):
+def _dump_model(version_id: str, classifier, vectorizer: CountVectorizer):
+    vectorizer_file_name = f"vectorizer_{version_id}.pkl"
+    classifier_file_name = f"linear_svc_model_{version_id}.pkl"
+    vectorizer_path = f"{os.getenv('DATA_FOLDER_PATH')}/{vectorizer_file_name}"
+    classifier_path = f"{os.getenv('DATA_FOLDER_PATH')}/{classifier_file_name}"
+
+    joblib.dump(classifier, classifier_path)
+    joblib.dump(vectorizer, vectorizer_path)
+
+
+def _save_model_version_to_db(classifier_version: ClassifierVersion):
+    # Save model version to our postgres db
+    engine = create_engine(os.getenv('DB_CONNECTION_STRING'))
+    with Session(engine) as session:
+        classifier_version_db = ClassifierVersion.from_orm(classifier_version)
+        session.add(classifier_version_db)
+        session.commit()
+        session.refresh(classifier_version_db)
+
+    saved_version = ClassifierVersionRead(
+        model_id=classifier_version_db.id,
+        created_at=classifier_version_db.created_at
+    )
+    return saved_version
+
+
+def _retrain_classifier(db_con_str: str, table_name: str):
     print("Getting training data...")
-    training_data_df = get_training_data(db_con_str, table_name)
+    training_data_df = _get_training_data(db_con_str, table_name)
     # training_data_df = read_csv(_TRAINING_FILE_NAME, sep=_FILE_SEPARATOR)
     print(f"Count of training data: {len(training_data_df)}")
 
@@ -156,60 +191,93 @@ def retrain_classifier(db_con_str: str, table_name: str):
     classifier = LinearSVC().fit(x_train_tfidf, y_train)
     print("Classifier is ready.")
 
-    accuracy = get_model_accuracy(
+    print("Getting model accuracy...")
+    accuracy = _get_model_accuracy(
         classifier=classifier,
         vectorizer=vectorizer,
         x_test=x_test,
         y_test=y_test
     )
+    print(f"Model accuracy: {accuracy}")
 
-    files_date = datetime.now().date()
-    vectorizer_file_name = f"vectorizer-{files_date}.pkl"
-    classifier_file_name = f"linear-svc-model-{files_date}.pkl"
+    version_id = str(uuid4())
 
-    joblib.dump(vectorizer, vectorizer_file_name)
-    joblib.dump(classifier, classifier_file_name)
+    print("Dumping model locally...")
+    _dump_model(
+        version_id=version_id,
+        classifier=classifier,
+        vectorizer=vectorizer
+    )
+    print("Model dumped.")
+
+    classifier_version = ClassifierVersion(
+        id=version_id,
+        accuracy=accuracy,
+    )
+
+    print("Saving model version to db...")
+    result = _save_model_version_to_db(classifier_version)
+    print("Model version saved.")
+
+    return result
+
+
+def start_classifier_retraining(db_con_str: str, table_name: str):
+    queue = get_redis_queue()
+    job = queue.enqueue(
+        _retrain_classifier,
+        db_con_str,
+        table_name,
+        result_ttl=-1,
+        job_timeout=MAX_JOB_TIMEOUT,
+    )
+    return JobIdRead(job_id=job.id)
+
+
+def get_retraining_job_result(job_id: str):
+    job = get_job(job_id)
+
+    result = ClassifierRetrainingResult(
+        job_id=job_id,
+        status=job.get_status(refresh=True)
+    )
+
+    job_result = job.return_value(refresh=True)
+    if job_result is not None:
+        result.result = job_result
+
+    return result
 
 
 if __name__ == "__main__":
     from sshtunnel import SSHTunnelForwarder
 
-    username = "wv-bleschunovd"
-    jump_host_password = "$$R4Yt1w1nG52TBF"
-    msu_db_password = "MRi9yJ6vPObQStZv"
-    dbeaver_host = "192.168.0.238"
-
-    reg_template1 = r"\d\d\.\d\d\.\d\d\.?.+"
-    reg_template2 = r"\d\d\.\d\d\.?.+"
-
-    print("Connecting to Jump Host...")
     jump_host_tunnel = SSHTunnelForwarder(
         ("192.168.0.20", 1984),
-        ssh_username=username,
-        ssh_password=jump_host_password,
-        remote_bind_address=(dbeaver_host, 1984)
+        ssh_username="wv-bleschunovd",
+        ssh_password="$$R4Yt1w1nG52TBF",
+        remote_bind_address=("192.168.0.238", 1984)
     )
+    print("Connecting to jump host")
     jump_host_tunnel.start()
-    print("Connected.")
     jump_host_port = jump_host_tunnel.local_bind_port
 
-    print("Connecting to MSU DB...")
     msu_db_tunnel = SSHTunnelForwarder(
         ("localhost", jump_host_port),
-        ssh_username=username,
-        ssh_password=msu_db_password,
+        ssh_username="wv-bleschunovd",
+        ssh_password="MRi9yJ6vPObQStZv",
         remote_bind_address=("srv-dwh", 1433)
     )
     msu_db_tunnel.start()
-    print("Connected.")
+    print("Connecting to msu db tunnel")
     msu_db_port = str(msu_db_tunnel.local_bind_port)
 
     msu_db_connection_string = f"mssql+pyodbc://dwh_connector:Avt528796R001T@localhost:{msu_db_port}/dwh?driver=ODBC+Driver+17+for+SQL+Server"
+    msu_db_table_name = "us.СправочникНоменклатура"
 
-    start_time = datetime.now()
-    retrain_classifier(
-        db_con_str=msu_db_connection_string,
-        table_name="us.СправочникНоменклатура",
-    )
-    end_time = datetime.now()
-    print(f"Script work time: {end_time - start_time}")
+    # job_id = start_classifier_retraining(msu_db_connection_string, msu_db_table_name)
+    # print(job_id)
+
+    # print(_retrain_classifier(msu_db_connection_string, msu_db_table_name))
+
+    pass
