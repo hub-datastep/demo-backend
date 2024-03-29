e525e09e5f4f6940ebb164a542645090c46aaa94
Syrnnik <sakh.alexandr@gmail.com>
2024-03-29 17:47:13 +0300

    fix: move db funcs to repository

---
+++ src/repository/classifier_version_repository.py
@@ -0,0 +1,23 @@
+from sqlmodel import Session, select, not_
+
+from infra.database import engine
+from scheme.classifier_scheme import ClassifierVersion
+
+
+def _get_classifier_versions():
+    with Session(engine) as session:
+        st = select(ClassifierVersion) \
+            .where(not_(ClassifierVersion.is_deleted))
+        result = session.exec(st).all()
+
+    return list(result)
+
+
+def _delete_classifier_version_in_db(model_id: str):
+    # Soft delete classifier version in our postgres db
+    with Session(engine) as session:
+        classifier_version = session.get(ClassifierVersion, model_id)
+        if classifier_version:
+            classifier_version.is_deleted = True
+            session.add(classifier_version)
+            session.commit()
