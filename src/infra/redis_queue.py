ff49cb270480249d6947d74eeaf5f38519cdc06a
Syrnnik <sakh.alexandr@gmail.com>
2024-04-01 21:43:53 +0300

    fix: add queue name arg to get redis queue func

--- src/infra/redis_queue.py
+++ src/infra/redis_queue.py
@@ -8,9 +8,9 @@ from rq.queue import Queue
 MAX_JOB_TIMEOUT = 60 * 60 * 24
 
 
-def get_redis_queue():
+def get_redis_queue(name: str):
     redis = Redis(host=os.getenv("REDIS_HOST"), password=os.getenv("REDIS_PASSWORD"))
-    queue = Queue(name="nomenclature", connection=redis)
+    queue = Queue(name=name, connection=redis)
     return queue
 
 
