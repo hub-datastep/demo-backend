81485f664fe0fad711b552497e5caff7e85f5bcb
Syrnnik <sakh.alexandr@gmail.com>
2024-04-23 18:18:05 +0300

    fix: hide retrain classifier by views endpoint

--- src/controller/retrain_classifier/retrain_classifier_controller.py
+++ src/controller/retrain_classifier/retrain_classifier_controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi_versioning import version
 
-from model import retrain_classifier_model, retrain_classifier_by_views_model
+from model import retrain_classifier_model
 from model.auth_model import get_current_user
 from scheme.classifier_scheme import RetrainClassifierUpload, ClassifierRetrainingResult, ClassifierVersionRead
 from scheme.user_scheme import UserRead
@@ -10,7 +10,16 @@ from scheme.user_scheme import UserRead
 router = APIRouter()
 
 
-@router.post("/")
+@router.get("")
+@version(1)
+def get_classifier_versions(
+    *,
+    current_user: UserRead = Depends(get_current_user),
+) -> list[ClassifierVersionRead]:
+    return retrain_classifier_model.get_classifiers_list()
+
+
+@router.post("")
 @version(1)
 def retrain_classifier(
     *,
@@ -23,17 +32,17 @@ def retrain_classifier(
     )
 
 
-@router.post("/by_views")
-@version(1)
-def retrain_classifier_by_views(
-    *,
-    body: RetrainClassifierUpload,
-    current_user: UserRead = Depends(get_current_user),
-):
-    return retrain_classifier_by_views_model.start_classifier_retraining(
-        db_con_str=body.db_con_str,
-        table_name=body.table_name,
-    )
+# @router.post("/by_views")
+# @version(1)
+# def retrain_classifier_by_views(
+#     *,
+#     body: RetrainClassifierUpload,
+#     current_user: UserRead = Depends(get_current_user),
+# ):
+#     return retrain_classifier_by_views_model.start_classifier_retraining(
+#         db_con_str=body.db_con_str,
+#         table_name=body.table_name,
+#     )
 
 
 @router.post("/result")
@@ -44,12 +53,3 @@ def retrain_classifier_result(
     job_id: str
 ) -> ClassifierRetrainingResult:
     return retrain_classifier_model.get_retraining_job_result(job_id)
-
-
-@router.get("/")
-@version(1)
-def get_classifier_versions(
-    *,
-    current_user: UserRead = Depends(get_current_user),
-) -> list[ClassifierVersionRead]:
-    return retrain_classifier_model.get_classifiers_list()
