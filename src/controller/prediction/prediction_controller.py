1b9be631ceb2efe0017bcb5d79cc3870c1702db8
Syrnnik <sakh.alexandr@gmail.com>
2024-05-09 22:10:26 +0300

    fix: return prompt editing endpoints

--- src/controller/prediction/prediction_controller.py
+++ src/controller/prediction/prediction_controller.py
@@ -6,7 +6,7 @@ from infra.database import get_session
 from model import datastep_pdf_model
 from model.auth_model import get_current_user
 from model.datastep_model import datastep_get_prediction
-from repository import user_repository, tenant_repository
+from repository import tenant_repository
 from scheme.prediction_scheme import (
     DatabasePredictionQuery, DatabasePredictionRead,
     DocumentPredictionRead, DocumentPredictionQuery
@@ -26,7 +26,8 @@ async def get_database_prediction(
 ):
     """
     """
-    current_user_tenant_db = tenant_repository.get_tenant_by_id(session, current_user.tenants[0].id)
+    user_tenant_id = current_user.tenants[0].id
+    current_user_tenant_db = tenant_repository.get_tenant_by_id(session, user_tenant_id)
     return await datastep_get_prediction(query, current_user_tenant_db, current_user.database_prediction_config)
 
 
