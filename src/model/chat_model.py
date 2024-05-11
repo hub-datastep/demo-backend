9c158b3a0355bae70d7fc3587d84b98f90bf7d25
Syrnnik <sakh.alexandr@gmail.com>
2024-05-11 13:47:28 +0300

    refactor: chat getting and creating

---
+++ src/model/chat_model.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException
+from sqlmodel import Session
+
+from repository import chat_repository
+from scheme.chat_scheme import Chat
+
+
+def get_chat_by_user_id(session: Session, user_id: int) -> Chat | None:
+    chat_db = chat_repository.get_chat_by_user_id(session, user_id)
+
+    if not chat_db:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Chat with user_id {user_id} not found.",
+        )
+
+    return chat_db
