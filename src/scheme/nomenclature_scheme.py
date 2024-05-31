068e395cba982d79905aa864c33e6dd9bb6a04de
Syrnnik <sakh.alexandr@gmail.com>
2024-05-31 05:29:28 +0300

    feat: add nomenclatures params in result of mapping

--- src/scheme/nomenclature_scheme.py
+++ src/scheme/nomenclature_scheme.py
@@ -14,6 +14,7 @@ class MappingOneTargetRead(SQLModel):
     nomenclature_guid: str
     nomenclature: str
     similarity_score: float
+    nomenclature_params: list[dict]
 
 
 class MappingOneNomenclatureRead(SQLModel):
