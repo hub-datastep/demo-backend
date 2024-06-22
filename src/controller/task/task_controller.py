bd5bba0f203a764ba0cb1c32b802251f3871edb9
Syrnnik <sakh.alexandr@gmail.com>
2024-06-22 19:55:51 +0300

    try fix: cleanup rq queues

--- src/controller/task/task_controller.py
+++ src/controller/task/task_controller.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from fastapi_versioning import version
 
+from infra.redis_queue import QueuesEnum
 from model import task_model
 from model.auth_model import get_current_user
 from scheme.task_scheme import RQJob
@@ -15,7 +16,19 @@ def get_all_jobs(current_user: UserRead = Depends(get_current_user)) -> list[RQJ
     return task_model.get_all_jobs()
 
 
+@router.post("/{queue_name}")
+@version(1)
+def cleanup_queue(
+    queue_name: QueuesEnum,
+    current_user: UserRead = Depends(get_current_user),
+):
+    return task_model.cleanup_queue(queue_name.value)
+
+
 @router.delete("/stop_job/{job_id}")
 @version(1)
-def stop_job_by_id(job_id: str, current_user: UserRead = Depends(get_current_user)):
+def stop_job_by_id(
+    job_id: str,
+    current_user: UserRead = Depends(get_current_user),
+):
     return task_model.stop_job_by_id(job_id)
