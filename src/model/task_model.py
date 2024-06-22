a68aff97bf5ac9adc497dfbaced6ba0a69bac069
Syrnnik <sakh.alexandr@gmail.com>
2024-06-22 19:19:36 +0300

    fix: jobs scheme

--- src/model/task_model.py
+++ src/model/task_model.py
@@ -1,22 +1,39 @@
 from fastapi import HTTPException, status
 from rq.command import send_stop_job_command
 from rq.exceptions import InvalidJobOperation
-from rq.registry import StartedJobRegistry
+from rq.job import JobStatus
+from rq.registry import FinishedJobRegistry, StartedJobRegistry, ScheduledJobRegistry, FailedJobRegistry
 
 from infra.redis_queue import get_redis_connection, QueueName
+from scheme.task_scheme import RQJob
+
+REGISTRIES = [
+    (FinishedJobRegistry, JobStatus.FINISHED),
+    (StartedJobRegistry, JobStatus.STARTED),
+    (ScheduledJobRegistry, JobStatus.SCHEDULED),
+    (FailedJobRegistry, JobStatus.FAILED),
+]
 
 
-def get_all_active_jobs():
+def get_all_jobs() -> list[RQJob]:
     redis = get_redis_connection()
-    queues_list = [value for key, value in vars(QueueName).items() if not key.startswith('__')]
+    queues_list: list[str] = [value for key, value in vars(QueueName).items() if not key.startswith('__')]
 
-    jobs_ids = []
+    jobs_list = []
     for queue in queues_list:
-        registry = StartedJobRegistry(queue, connection=redis)
-        running_job_ids = registry.get_job_ids()
-        jobs_ids.extend(running_job_ids)
+        for Registry, registry_status in REGISTRIES:
+            registry = Registry(queue, connection=redis)
+            jobs_ids: list[str] = registry.get_job_ids()
 
-    return jobs_ids
+            for job_id in jobs_ids:
+                rq_job = RQJob(
+                    job_id=job_id,
+                    status=registry_status,
+                    queue=queue,
+                )
+                jobs_list.append(rq_job)
+
+    return jobs_list
 
 
 def stop_job_by_id(job_id: str):
