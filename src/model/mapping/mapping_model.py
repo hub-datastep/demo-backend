80cc3a5779810a126ad78f3bcee966d0acdc4340
Syrnnik <sakh.alexandr@gmail.com>
2024-07-13 02:19:11 +0300

    feat: check if collection name, classifier model id is set & collection exists in vectorstore

--- src/model/mapping/mapping_model.py
+++ src/model/mapping/mapping_model.py
@@ -1,19 +1,19 @@
-from pathlib import Path
-
 import joblib
 import numpy as np
 from chromadb import QueryResult
 from chromadb.api.models.Collection import Collection
+from fastapi import HTTPException, status
 from fastembed import TextEmbedding
 from pandas import DataFrame
 from rq import get_current_job
 from rq.job import JobStatus
 from tqdm import tqdm
 
-from infra.chroma_store import connect_to_chroma_collection
+from infra.chroma_store import connect_to_chroma_collection, get_all_collections
 from infra.redis_queue import get_redis_queue, MAX_JOB_TIMEOUT, QueueName, get_job
 from model.classifier.classifier_retrain_model import TRAINING_COLUMNS
 from model.classifier.classifier_version_model import get_model_path
+from model.ner.ner import HTTP_NER
 from scheme.classifier.classifier_config_scheme import ClassifierConfig
 from scheme.mapping.mapping_scheme import MappingOneNomenclatureUpload, \
     MappingNomenclaturesResultRead, MappingOneTargetRead, MappingOneNomenclatureRead
@@ -21,14 +21,10 @@ from scheme.task.task_scheme import JobIdRead
 from util.extract_keyword import extract_keyword
 from util.features_extraction import extract_features, get_noms_metadatas_with_features
 from util.normalize_name import normalize_name
-from model.ner.ner import HTTP_NER
-
 
 tqdm.pandas()
 # noinspection PyTypeChecker
 np.set_printoptions(threshold=np.inf)
-
-SIMILAR_NOMS_COUNT = 3
 
 
 def get_nomenclatures_groups(
@@ -54,10 +50,10 @@ def map_on_nom(
     collection: Collection,
     nom_embeddings: np.ndarray,
     group: str,
-    most_similar_count: int,
     metadatas_list: list[dict],
     is_hard_params: bool,
     use_params: bool,
+    most_similar_count: int = 1,
 ) -> list[MappingOneTargetRead] | None:
     # Just sure that group is str
     group = str(group)
@@ -118,7 +114,7 @@ def get_nomenclatures_embeddings(strings: list[str]) -> list[np.ndarray]:
 def split_nomenclatures_by_chunks(
     nomenclatures: list[MappingOneNomenclatureUpload],
     chunk_size: int = 100,
-) -> list[list[MappingOneNomenclatureUpload]]: # type: ignore
+) -> list[list[MappingOneNomenclatureUpload]]:
     for i in range(0, len(nomenclatures), chunk_size):
         yield nomenclatures[i:i + chunk_size]
 
@@ -127,9 +123,6 @@ def create_mapping_job(
     nomenclatures: list[MappingOneNomenclatureUpload],
     previous_job_id: str | None,
     most_similar_count: int,
-    chroma_collection_name: str,
-    model_id: str,
-    use_params: bool,
     classifier_config: ClassifierConfig | None,
 ) -> JobIdRead:
     queue = get_redis_queue(name=QueueName.MAPPING)
@@ -137,9 +130,6 @@ def create_mapping_job(
         _map_nomenclatures_chunk,
         nomenclatures,
         most_similar_count,
-        chroma_collection_name,
-        model_id,
-        use_params,
         classifier_config,
         meta={
             "previous_nomenclature_id": previous_job_id
@@ -153,12 +143,37 @@ def create_mapping_job(
 def start_mapping(
     nomenclatures: list[MappingOneNomenclatureUpload],
     most_similar_count: int,
-    chroma_collection_name: str,
     chunk_size: int,
-    model_id: str,
-    use_params: bool,
     classifier_config: ClassifierConfig | None,
 ) -> JobIdRead:
+    # Check if collection name exists in user's classifier config
+    collection_name = classifier_config.chroma_collection_name
+    is_collection_name_exists_in_config = bool(collection_name)
+    if not is_collection_name_exists_in_config:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Collection name is not set in classifier config.",
+        )
+
+    # Check if collection exists in vectorstore
+    collections_list = get_all_collections()
+    is_collection_exists_in_vectorstore = collection_name in collections_list
+    if not is_collection_exists_in_vectorstore:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Collection {collection_name} is not exists in vectorstore.",
+        )
+
+    # Check if classifier version id exists in user's classifier config
+    model_id = classifier_config.model_id
+    is_model_id_exists_in_config = bool(model_id)
+    if not is_model_id_exists_in_config:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Classifier version {model_id} is not exists.",
+        )
+
+    # Split nomenclatures to chunks
     segments = split_nomenclatures_by_chunks(
         nomenclatures=nomenclatures,
         chunk_size=chunk_size,
@@ -169,9 +184,6 @@ def start_mapping(
             nomenclatures=segment,
             previous_job_id=last_job_id,
             most_similar_count=most_similar_count,
-            chroma_collection_name=chroma_collection_name,
-            model_id=model_id,
-            use_params=use_params,
             classifier_config=classifier_config,
         )
         last_job_id = job.job_id
@@ -182,30 +194,24 @@ def start_mapping(
 def _map_nomenclatures_chunk(
     nomenclatures: list[MappingOneNomenclatureUpload],
     most_similar_count: int,
-    chroma_collection_name: str,
-    model_id: str,
-    use_params: bool,
     classifier_config: ClassifierConfig | None,
 ) -> list[MappingOneNomenclatureRead]:
     job = get_current_job()
 
-    # Convert nomenclatures to DataFrame
-    noms = convert_nomenclatures_to_df(nomenclatures)
-
-    job.meta['total_count'] = len(noms)
+    job.meta['total_count'] = len(nomenclatures)
     job.meta['ready_count'] = 0
     job.save_meta()
+
+    # Convert nomenclatures to DataFrame
+    noms = convert_nomenclatures_to_df(nomenclatures)
 
     # Normalize nomenclatures names
     noms['normalized'] = noms['nomenclature'].progress_apply(
         lambda nom_name: normalize_name(nom_name)
     )
 
+    # Extract nomenclature keyword
     is_use_keywords = classifier_config is None or classifier_config.is_use_keywords_detection
-
-    #TODO: сделать проверку на пустоту в названии хрома коллекции в контроллере и еще проверку на существование такой коллекции
-    classifier_config_chroma_collection = "" if classifier_config is None else classifier_config.chroma_collection_name
-
     if is_use_keywords:
         noms['keyword'] = noms['normalized'].progress_apply(
             lambda nom_name: extract_keyword(nom_name)
@@ -220,25 +226,28 @@ def _map_nomenclatures_chunk(
     noms['name'] = noms['nomenclature']
     noms['brand'] = HTTP_NER().predict(noms['nomenclature'].to_list())
 
-    # Извлечение характеристик и добавление их в метаданные
+    # Extract all noms params
     noms = extract_features(noms)
 
-    # Classification to get mapping group
+    # Run classification to get mapping group
+    model_id = classifier_config.model_id
+    use_params = classifier_config is None or classifier_config.use_params
     noms['group'] = get_nomenclatures_groups(
         noms=noms,
         model_id=model_id,
         use_params=use_params,
     )
 
-    # Получаем метаданные всех номенклатур с характеристиками
+    # Get all noms params with group as metadatas list
     noms['metadata'] = get_noms_metadatas_with_features(noms)
 
-    collection = connect_to_chroma_collection(collection_name=classifier_config_chroma_collection)
+    collection_name = classifier_config.chroma_collection_name
+    collection = connect_to_chroma_collection(collection_name)
 
     noms['mappings'] = None
     noms['similar_mappings'] = None
     noms['nomenclature_params'] = None
-    
+
     for i, nom in noms.iterrows():
         # Create mapping metadatas list for query
         metadatas_list = []
@@ -247,6 +256,7 @@ def _map_nomenclatures_chunk(
             if val != "":
                 metadatas_list.append({str(key): val})
 
+        # Save metadatas in df for response
         nom['nomenclature_params'] = metadatas_list
 
         # Check if nom really belong to mapped group, only if is_use_keywords is True
@@ -262,7 +272,6 @@ def _map_nomenclatures_chunk(
                 collection=collection,
                 nom_embeddings=nom['embeddings'],
                 group=nom['group'],
-                most_similar_count=most_similar_count,
                 metadatas_list=metadatas_list,
                 is_hard_params=True,
                 use_params=use_params,
@@ -275,7 +284,7 @@ def _map_nomenclatures_chunk(
                     collection=collection,
                     nom_embeddings=nom['embeddings'],
                     group=nom['group'],
-                    most_similar_count=SIMILAR_NOMS_COUNT,
+                    most_similar_count=most_similar_count,
                     metadatas_list=metadatas_list,
                     is_hard_params=False,
                     use_params=use_params,
