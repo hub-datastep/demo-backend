f066c9047197811a0f8d1eb4517101f06f7fca67
IT-Kitty <katerina.kvtn.skh@gmail.com>
2024-07-18 18:32:13 +0300

    feat: add saving mapping results

---
+++ src/scheme/mapping/mapping_results_scheme.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlmodel import SQLModel, Field
+
+
+class CustomConfig:
+    arbitrary_types_allowed = True
+
+
+class MappingResult(SQLModel, table=True):
+    __tablename__ = "mapping_result"
+    Config = CustomConfig
+
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    mapping_result: dict = Field(sa_column=Column(JSONB))
+    created_at: datetime | None = Field(default=datetime.utcnow())
+    mapping_nomenclature_corrected: str | None = Field(default=None)
