4bc9fb3f0d034d061b9aa0c00aecc642fef8252a
Syrnnik <sakh.alexandr@gmail.com>
2024-08-04 02:38:49 +0300

    feat: get mode by id with tenants list

--- src/controller/mode/mode_controller.py
+++ src/controller/mode/mode_controller.py
@@ -5,11 +5,25 @@ from sqlmodel import Session
 from infra.database import get_session
 from middleware.role_middleware import admins_only
 from model.auth.auth_model import get_current_user
+from model.mode import mode_model
 from repository.mode import mode_repository
 from scheme.mode.mode_scheme import ModeRead, ModeCreate
 from scheme.user.user_scheme import UserRead
 
 router = APIRouter()
+
+
+@router.get("/{mode_id}", response_model=ModeRead)
+@version(1)
+@admins_only
+def get_mode_by_id(
+    mode_id: int,
+    current_user: UserRead = Depends(get_current_user),
+    session: Session = Depends(get_session),
+):
+    """
+    """
+    return mode_model.get_mode_by_id(session, mode_id)
 
 
 @router.post("", response_model=ModeRead)
