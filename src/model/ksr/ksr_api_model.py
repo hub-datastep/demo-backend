b7934304b6d68b04512bf52ecee10e548922a8fb
IT-Kitty <katerina.kvtn.skh@gmail.com>
2024-08-07 01:20:06 +0300

    feat: add new columns

--- src/model/ksr/ksr_api_model.py
+++ src/model/ksr/ksr_api_model.py
@@ -14,17 +14,19 @@ def get_ksr(id_ksr: str) -> dict:
         return {"error": "No data found for the given id_ksr"}
 
     row = ksr_nomenclature.iloc[0]
-    id_okpd = row['id_okpd']
     name_ksr = row['name']
+    group_id = row['group_id']
+    group_name = row['group_name']
     units_of_measurement = row['units_of_measure']
 
     features = {col: row[col] for col in ksr_nomenclature.columns
-                if notna(row[col]) and col not in ['id_ksr', 'id_okpd', 'name', 'units_of_measure']}
+                if notna(row[col]) and row[col] != "" and col not in ['id_ksr', 'group_id', 'group_name', 'name', 'units_of_measure']}
 
     return {
-        "id_ОКПД": id_okpd,
         "id_КСР": id_ksr,
-        "Название": name_ksr,
-        "Параметры": features,
-        "Ед. изм.": units_of_measurement
+        "group_id": group_id,
+        "group_name": group_name,
+        "name_ksr": name_ksr,
+        "parameters": features,
+        "units_of_measurement": units_of_measurement,
     }
