1bf5873fd699f27f4ab7d8484ecb189a8f65bc91
Syrnnik <sakh.alexandr@gmail.com>
2024-08-07 20:47:05 +0300

    fix: NER req chunking

--- src/model/ner/ner.py
+++ src/model/ner/ner.py
@@ -3,19 +3,20 @@ import httpx
 from infra.env import NER_SERVICE_URL
 
 
-class HTTP_NER:
+class NERServiceClient:
     def __init__(self, url: str = NER_SERVICE_URL, chunk_size: int = 100, timeout: int = 100):
-        self.__client = httpx.Client()
-        self.__chunk_size = chunk_size
-        self.__timeout = timeout
-        self.__url = url
+        self._client = httpx.Client()
+        self._chunk_size = chunk_size
+        self._timeout = timeout
+        self._url = url
 
     def predict(self, nomenclatures: list[str]) -> list[str]:
         noms_brands_list = []
 
         nomenclatures_count = len(nomenclatures)
-        for i in range(0, nomenclatures_count, self.__chunk_size):
-            noms_chunk = nomenclatures[i:self.__chunk_size]
+        print(f"Nomenclatures count: {nomenclatures_count}")
+        for i in range(0, nomenclatures_count, self._chunk_size):
+            noms_chunk = nomenclatures[i:i + self._chunk_size]
 
             if len(noms_chunk) == 0:
                 break
@@ -24,10 +25,10 @@ class HTTP_NER:
                 body = {
                     "nomenclatures": noms_chunk,
                 }
-                response = self.__client.post(
-                    self.__url,
+                response = self._client.post(
+                    url=self._url,
                     json=body,
-                    timeout=self.__timeout,
+                    timeout=self._timeout,
                 )
                 response_json = response.json()
                 response_brands = response_json['brands']
@@ -36,10 +37,11 @@ class HTTP_NER:
             except Exception as e:
                 print(f"Index chunk {i} - {e}")
 
+        print(f"Nomenclatures brands count: {len(noms_brands_list)}")
         return noms_brands_list
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.__client.close()
+        self._client.close()
 
 
-ner_service = HTTP_NER()
+ner_service = NERServiceClient()
