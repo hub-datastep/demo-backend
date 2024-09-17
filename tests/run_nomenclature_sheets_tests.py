4dc73e1aea7e32c0702c7ccf907a4d45f29c6926
Syrnnik <sakh.alexandr@gmail.com>
2024-09-17 19:45:17 +0300

    fix: mapping tests

--- tests/run_nomenclature_sheets_tests.py
+++ tests/run_nomenclature_sheets_tests.py
@@ -9,53 +9,47 @@ from utils.result_mapper import process_results
 
 
 def run_tests():
-    try:
-        logger.info("Получение тест кейсов.")
-        test_cases = get_test_cases()
-        test_cases_count = len(test_cases)
-        logger.info(f"Test cases count: {test_cases_count}")
+    logger.info("Получение тест кейсов.")
+    test_cases = get_test_cases()
+    test_cases_count = len(test_cases)
+    logger.info(f"Test cases count: {test_cases_count}")
 
-        logger.info("Авторизация в API.")
-        token = authenticate()
+    logger.info("Авторизация в API.")
+    token = authenticate()
 
-        logger.info("Запуск маппинга номенклатур.")
-        job_id = start_nomenclature_mapping(test_cases, token)
+    logger.info("Запуск маппинга номенклатур.")
+    job_id = start_nomenclature_mapping(test_cases, token)
 
-        if job_id:
-            logger.info(f"Job ID: {job_id}")
-            logger.info("Ожидание завершения задачи маппинга.")
+    if job_id:
+        logger.info(f"Job ID: {job_id}")
+        logger.info("Ожидание завершения задачи маппинга.")
 
-            # Одна номенклатура обрабатывается примерно 5-10 секунд
-            # Поэтому рассчитываем время получения результата
-            # относительно кол-ва номенклатур
-            # Фетчим результат каждые примерно 50 номенклатур
-            result_fetch_interval = test_cases_count / 50 * 5
-            # result_fetch_interval = 30
+        # Одна номенклатура обрабатывается примерно 15-30 секунд
+        # Поэтому рассчитываем время получения результата
+        # относительно кол-ва номенклатур
+        # Фетчим результат каждые примерно 50 номенклатур
+        result_fetch_interval = test_cases_count / 50 * 30
+        # result_fetch_interval = 30
 
-            # Ожидание завершения задачи
-            result = wait_for_job_completion(job_id, token, interval=result_fetch_interval)
-            logger.info(f"Mapping results count: {len(result)}")
+        # Ожидание завершения задачи
+        result = wait_for_job_completion(job_id, token, interval=result_fetch_interval)
+        logger.info(f"Mapping results count: {len(result)}")
 
-            if result:
-                logger.info("Результаты маппинга получены успешно.")
-                logger.info(result)
+        if result:
+            logger.info("Результаты маппинга получены успешно.")
+            logger.info(result)
 
-                logger.info("Предобработка результатов.")
-                processed_results = process_results(test_cases, result)
+            logger.info("Предобработка результатов.")
+            processed_results = process_results(test_cases, result)
 
-                # Создание уникального имени для нового листа без двоеточий
-                tests_datetime = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
-                new_sheet_name = f"Mapping Results {tests_datetime}"
+            # Создание уникального имени для нового листа без двоеточий
+            tests_datetime = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+            new_sheet_name = f"Mapping Results {tests_datetime}"
 
-                logger.info(f"Сохранение результатов в новый лист: {new_sheet_name}.")
-                create_new_offline_sheet_and_write_results(new_sheet_name, processed_results)
-            else:
-                logger.error("Не удалось получить результаты маппинга.")
+            logger.info(f"Сохранение результатов в новый лист: {new_sheet_name}.")
+            create_new_offline_sheet_and_write_results(new_sheet_name, processed_results)
         else:
-            logger.error("Не удалось запустить маппинг номенклатур.")
-    except Exception as e:
-        logger.error(f"Произошла ошибка: {e}")
-        logger.error("Подробности:", exc_info=True)
+            logger.error("Не удалось получить результаты маппинга.")
 
 
 if __name__ == "__main__":
