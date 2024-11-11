3e65699f9176862694629773acd0110985053367
Syrnnik <sakh.alexandr@gmail.com>
2024-11-11 18:32:44 +0300

    fix: default config user id

--- src/repository/emergency_class/emergency_classification_config_repository.py
+++ src/repository/emergency_class/emergency_classification_config_repository.py
@@ -1,15 +1,18 @@
 from sqlmodel import Session, select
 
 from infra.database import engine
-from scheme.emergency_class.emergency_classification_config_scheme import EmergencyClassificationConfig
+from scheme.emergency_class.emergency_classification_config_scheme import (
+    EmergencyClassificationConfig,
+)
 
 # ID of empty user
-DEFAULT_CONFIG_USER_ID = 8
+DEFAULT_CONFIG_USER_ID = 0
+# For tests
+# DEFAULT_CONFIG_USER_ID = 8
 
 
 def get_default_config() -> EmergencyClassificationConfig | None:
     with Session(engine) as session:
-        # config = session.get(EmergencyClassificationConfig, DEFAULT_CONFIG_USER_ID)
         st = select(EmergencyClassificationConfig)
         st = st.where(EmergencyClassificationConfig.user_id == DEFAULT_CONFIG_USER_ID)
 
@@ -18,7 +21,10 @@ def get_default_config() -> EmergencyClassificationConfig | None:
         return config
 
 
-def get_config_by_user_id(session: Session, user_id: int) -> EmergencyClassificationConfig | None:
+def get_config_by_user_id(
+    session: Session,
+    user_id: int,
+) -> EmergencyClassificationConfig | None:
     st = select(EmergencyClassificationConfig)
     st = st.where(EmergencyClassificationConfig.user_id == user_id)
     config = session.exec(st).first()
