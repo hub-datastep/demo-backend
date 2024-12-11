6fac9fb67f1204ddc189b238ff0070bc2264e412
lanutrix <dmodv@vk.com>
2024-12-11 16:41:34 +0300

    feat mapping_execute

--- par_job.py
+++ par_job.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException
 from scheme.parsing.parsing_scheme import UploadCardRequest, UploadCardResponse, Material
 from model.parsing.parsing_utd import parsing_utd_file
 from model.parsing.download_pdf import download_pdf
-from model.parsing.webhook import kafka_request
+from mapping_execute import Mapping
 
 
 def ParsingException(request:UploadCardRequest, text:str):
@@ -44,7 +44,8 @@ def parsing_job(request: UploadCardRequest) -> bool:
         
         print(nomenclatures)
         # Тут должна быть логика маппинга
-
+        answer = Mapping(nomenclatures).execute()
+        print(answer)
 
         # Допустим, распознано всё, кроме некоторых данных
         response = UploadCardResponse(
