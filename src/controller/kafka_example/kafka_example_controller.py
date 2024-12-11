1fb6745ee9bae86b98f247a178e82457dedaec8c
funkylen <funkylen@gmail.com>
2024-12-11 14:35:09 +0300

    feat: kafka improve

--- src/controller/kafka_example/kafka_example_controller.py
+++ src/controller/kafka_example/kafka_example_controller.py
@@ -10,5 +10,8 @@ router = APIRouter()
 @version(1)
 @with_broker_connection
 async def index():
-    await broker.publish({"key": 'hello world'}, topic=TGBOT_DELIVERY_NOTE_TOPIC)
+    try:
+        await broker.publish({"key": 'hello world'}, topic=TGBOT_DELIVERY_NOTE_TOPIC)
+    except Exception as e:
+        print(f"Ошибка при публикации сообщения: {e}")
     return 'Hello world!'
