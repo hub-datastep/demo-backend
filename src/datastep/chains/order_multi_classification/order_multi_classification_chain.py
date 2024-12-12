ee3c89c7414e32ba85defa62046c46c2ebb1fd38
Syrnnik <sakh.alexandr@gmail.com>
2024-12-13 01:02:27 +0300

    feat: skip classification if not needed

--- src/datastep/chains/order_multi_classification/order_multi_classification_chain.py
+++ src/datastep/chains/order_multi_classification/order_multi_classification_chain.py
@@ -1,16 +1,19 @@
 from langchain_openai import AzureChatOpenAI
+from loguru import logger
 
 from datastep.chains.order_classification_chain import get_llm_by_client_credentials
 from datastep.chains.order_multi_classification.class_score_chain import get_class_score_chain, get_class_score
 from datastep.chains.order_multi_classification.most_relevant_class_chain import get_most_relevant_class, \
     get_most_relevant_class_chain
+from scheme.order_classification.order_classification_config_scheme import RulesWithParams
 from scheme.order_classification.order_classification_scheme import MostRelevantClassLLMResponse
 
 
 def _get_scores_of_classes(
     llm: AzureChatOpenAI,
     order_query: str,
-    rules_by_class: dict,
+    rules_by_classes: dict,
+    client: str | None = None,
     verbose: bool = False,
 ) -> dict[str, str]:
     score_chain = get_class_score_chain(
@@ -19,12 +22,20 @@ def _get_scores_of_classes(
     )
 
     scores = {}
-    for class_name, rules in rules_by_class.items():
+    for class_name, params in rules_by_classes.items():
+        rules_with_params = RulesWithParams(**params)
+
+        if not rules_with_params.is_use_classification:
+            logger.info(f"Class '{class_name}' was skipped by config of client '{client}'")
+            continue
+
+        rules = rules_with_params.rules
         score = get_class_score(
             chain=score_chain,
             order_query=order_query,
             class_name=class_name,
             rules=rules,
+            client=client,
         )
         scores[class_name] = score
 
@@ -33,7 +44,7 @@ def _get_scores_of_classes(
 
 def get_order_class(
     order_query: str,
-    rules_by_class: dict,
+    rules_by_classes: dict,
     client: str | None = None,
     verbose: bool = False,
 ) -> MostRelevantClassLLMResponse:
@@ -42,7 +53,8 @@ def get_order_class(
     scores = _get_scores_of_classes(
         llm=llm,
         order_query=order_query,
-        rules_by_class=rules_by_class,
+        rules_by_classes=rules_by_classes,
+        client=client,
         verbose=verbose,
     )
 
@@ -59,7 +71,8 @@ def get_order_class(
     order_class = get_most_relevant_class(
         chain=most_relevant_class_chain,
         order_query=order_query,
-        scores=scores_str
+        scores=scores_str,
+        client=client,
     )
 
     return order_class
