61e28a8dd5d354b319a4003e75ef88ab92630401
Syrnnik <sakh.alexandr@gmail.com>
2024-12-14 00:31:19 +0300

    refactor: kafka broker

--- src/infra/kafka.py
+++ src/infra/kafka.py
@@ -1,50 +1,40 @@
-import ssl
-from functools import wraps
-from pathlib import Path
+from ssl import create_default_context, Purpose, CERT_REQUIRED
 
 from faststream.kafka import KafkaBroker, KafkaRouter
 from faststream.security import SASLPlaintext
 
-from infra.env import KAFKA_BOOTSTRAP_SERVERS, KAFKA_USERNAME, KAFKA_PASSWORD, KAFKA_WITH_SSL
+from infra.env import KAFKA_BOOTSTRAP_SERVERS, KAFKA_USERNAME, KAFKA_PASSWORD, IS_KAFKA_WITH_SSL, DATA_FOLDER_PATH
+
+kafka_router = KafkaRouter()
+
+brokers_list = KAFKA_BOOTSTRAP_SERVERS.split(",")
+
+SSL_CERT_PATH = f"{DATA_FOLDER_PATH}/ssl-certs/unistroy-ca-cert.pem"
 
 
-def create_broker():
-    kafka_router = KafkaRouter()
-
-    brokers = KAFKA_BOOTSTRAP_SERVERS.split(",")
-
-    if KAFKA_WITH_SSL:
-        ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+def create_kafka_broker():
+    security = None
+    # Init Kafka Security if needed
+    if IS_KAFKA_WITH_SSL:
+        ssl_context = create_default_context(Purpose.SERVER_AUTH)
         ssl_context.check_hostname = False
-        ssl_context.verify_mode = ssl.CERT_REQUIRED
-        # NOTE: Путь до сертификата. Возможно, есть смысл хранить в env или поменять с корня проекта
-        path_to_cert = (Path(__file__).parent / ".." / ".." / "ca-cert.pem").resolve()
-        ssl_context.load_verify_locations(cafile=path_to_cert)
+        ssl_context.verify_mode = CERT_REQUIRED
+        ssl_context.load_verify_locations(cafile=SSL_CERT_PATH)
         security = SASLPlaintext(
-            ssl_context=ssl_context, username=KAFKA_USERNAME, password=KAFKA_PASSWORD
+            ssl_context=ssl_context,
+            username=KAFKA_USERNAME,
+            password=KAFKA_PASSWORD,
         )
-        kafka_broker = KafkaBroker(brokers, security=security)
-    else:
-        kafka_broker = KafkaBroker(brokers)
+
+    # Init Kafka Broker
+    kafka_broker = KafkaBroker(
+        brokers_list,
+        security=security,
+    )
 
     kafka_broker.include_router(router=kafka_router)
 
     return kafka_broker
 
 
-def with_broker_connection(func):
-    """
-    Декоратор, чтобы не прописывать руками connect/close
-    """
-
-    @wraps(func)
-    async def wrapper_func(*args, **kwargs):
-        await broker.connect()
-        result = await func(*args, **kwargs)
-        await broker.close()
-        return result
-
-    return wrapper_func
-
-
-broker = create_broker()
+kafka_broker = create_kafka_broker()
