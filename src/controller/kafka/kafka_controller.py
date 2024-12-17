1172f60368c68ed0f8dea48adeabe33284b75f1b
Syrnnik <sakh.alexandr@gmail.com>
2024-12-17 19:22:06 +0300

    refactor: kafka test controller

--- src/controller/kafka/kafka_controller.py
+++ src/controller/kafka/kafka_controller.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi_versioning import version
 
 from infra.env import TGBOT_DELIVERY_NOTE_TOPIC
 from infra.kafka import kafka_broker
 from middleware.kafka_middleware import with_kafka_broker_connection
+from model.auth.auth_model import get_current_user
+from scheme.user.user_scheme import UserRead
 
 router = APIRouter()
 
@@ -11,7 +13,9 @@ router = APIRouter()
 @router.get("/test")
 @version(1)
 @with_kafka_broker_connection
-async def send_message_to_kafka():
+async def send_test_message_to_kafka(
+    current_user: UserRead = Depends(get_current_user),
+):
     message = "Hello World"
 
     await kafka_broker.publish(
