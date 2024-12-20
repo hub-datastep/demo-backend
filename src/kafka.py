c1239e9ae092fb7c5289b674368820ba2502c0be
Syrnnik <sakh.alexandr@gmail.com>
2024-12-20 11:23:46 +0300

    feat: iterative parsing and mapping starting

--- src/kafka.py
+++ src/kafka.py
@@ -1,7 +1,11 @@
 from faststream import FastStream
 from loguru import logger
 
-from infra.env import KAFKA_CONSUMER_GROUP, TGBOT_DELIVERY_NOTE_TOPIC, TGBOT_DELIVERY_NOTE_EXPORT_TOPIC
+from infra.env import (
+    KAFKA_CONSUMER_GROUP,
+    TGBOT_DELIVERY_NOTE_TOPIC,
+    TGBOT_DELIVERY_NOTE_EXPORT_TOPIC,
+)
 from infra.kafka import kafka_broker, send_message_to_kafka
 from model.mapping import mapping_with_parsing_model
 from scheme.file.utd_card_message_scheme import UTDCardInputMessage
@@ -28,14 +32,16 @@ async def unistroy_mapping_with_parsing_consumer(body: UTDCardInputMessage):
     logger.debug(f"Unistroy Kafka Request (input message):\n{body}")
 
     # Run mapping with parsing and wait results
-    output_message = mapping_with_parsing_model.parse_and_map_utd_card(body=body)
+    output_messages_list = mapping_with_parsing_model.parse_and_map_utd_card(body=body)
 
-    # Send message to Unistroy Kafka export-topic with results
-    logger.debug(f"Unistroy Kafka Response (output message):\n{output_message}")
-    await send_message_to_kafka(
-        message_body=output_message.dict(),
-        topic=TGBOT_DELIVERY_NOTE_EXPORT_TOPIC,
-    )
+    async for output_message in output_messages_list:
+        logger.debug(f"Unistroy Kafka Response (output message):\n{output_message}")
+
+        # Send message to Unistroy Kafka export-topic with results
+        await send_message_to_kafka(
+            message_body=output_message.dict(),
+            topic=TGBOT_DELIVERY_NOTE_EXPORT_TOPIC,
+        )
 
 # ! У нас нет пока нет прав на чтение этого топика, только на запись
 # @kafka_broker.subscriber(
--- src/model/mapping/mapping_with_parsing_model.py
+++ src/model/mapping/mapping_with_parsing_model.py
@@ -1,4 +1,5 @@
 from datetime import date
+from typing import Any, AsyncGenerator
 
 from fastapi import HTTPException
 from loguru import logger
@@ -6,7 +7,9 @@ from loguru import logger
 from exception.utd_card_processing_exception import raise_utd_card_processing_exception
 from model.file.utd.download_pdf_model import download_file
 from model.file.utd.mappings_with_parsed_data_model import add_parsed_data_to_mappings
-from model.file.utd.pdf_parsing_model import extract_params_and_noms
+from model.file.utd.multi_entities_pdf_parsing_model import (
+    extract_entities_with_params_and_noms,
+)
 from model.mapping.mapping_execute_model import (
     get_noms_with_indexes,
     start_mapping_and_wait_results,
@@ -19,10 +22,14 @@ from scheme.file.utd_card_message_scheme import (
 )
 from util.uuid import generate_uuid
 
-UNISTROY_USER_ID = 56
+# UNISTROY_USER_ID = 56
+# For tests
+UNISTROY_USER_ID = 1
 
 
-def parse_and_map_utd_card(body: UTDCardInputMessage) -> UTDCardOutputMessage:
+async def parse_and_map_utd_card(
+    body: UTDCardInputMessage,
+) -> AsyncGenerator[UTDCardOutputMessage, Any]:
     try:
         first_utd_doc = body.documents[0]
         pdf_file_url = first_utd_doc.idn_link
@@ -32,64 +39,68 @@ def parse_and_map_utd_card(body: UTDCardInputMessage) -> UTDCardOutputMessage:
         file_bytes = download_file(file_url=pdf_file_url)
 
         # Parse params and nomenclatures from UTD pdf file
-        params_and_noms = extract_params_and_noms(
-            pdf_file_content=file_bytes,
+        utd_entities_with_params_and_noms = extract_entities_with_params_and_noms(
+            pdf_file=file_bytes,
             idn_file_guid=idn_file_guid,
         )
-        logger.debug(f"Extracted params and noms:\n{params_and_noms}")
-        nomenclatures_list = params_and_noms.nomenclatures_list
-        nomenclatures_with_indexes_list = get_noms_with_indexes(nomenclatures_list)
 
         # Get Unistroy user and its params
         user = user_model.get_full_user_by_id(user_id=UNISTROY_USER_ID)
         classifier_config = user.classifier_config
         tenant_id = user.tenant_id
 
-        # Generate mapping iteration key (UTD guid)
-        iteration_key = generate_uuid()
+        for utd_entity in utd_entities_with_params_and_noms:
+            logger.debug(f"UTD Entity '{utd_entity.idn_number}' with params and noms:\n{utd_entity}")
 
-        # Start mapping and wait results
-        mapping_results = start_mapping_and_wait_results(
-            nomenclatures_list=nomenclatures_with_indexes_list,
-            classifier_config=classifier_config,
-            tenant_id=tenant_id,
-            iteration_key=iteration_key,
-        )
-        logger.debug(f"Mapping Results:\n{mapping_results}")
+            # Generate mapping iteration key (UTD guid)
+            iteration_key = generate_uuid()
 
-        mapped_materials = add_parsed_data_to_mappings(
-            mapping_results=mapping_results,
-            # TODO: pass materials data from UTD
-            parsed_materials_data=[],
-        )
+            # Set index for each nomenclature
+            nomenclatures_list = utd_entity.nomenclatures_list
+            nomenclatures_with_indexes_list = get_noms_with_indexes(nomenclatures_list)
 
-        output_message = UTDCardOutputMessage(
-            **body.dict(exclude={"guid"}),
-            idn_card_guid=body.guid,
-            guid=iteration_key,
-            status=UTDCardStatus.DONE,
-            # Mapping Data
-            materials=mapped_materials,
-            # Parsed Data
-            supplier_inn=params_and_noms.seller_inn,
-            idn_number=params_and_noms.invoice_number,
-            idn_date=params_and_noms.invoice_date,
-            # TODO: set parsed params from UTD pdf file
-            # ! Now it's mocked data
-            organization_inn="3305061878",
-            correction_idn_number="НО-12865РД/2",
-            correction_idn_date=date(2024, 8, 27),
-            contract_name="ДОГОВОР ПОСТАВКИ № 003/06-Лето от 13.09.2023",
-            contract_number="003/06-Лето",
-            contract_date=date(2024, 8, 27),
-        )
+            # Start mapping and wait results
+            mapping_results = start_mapping_and_wait_results(
+                nomenclatures_list=nomenclatures_with_indexes_list,
+                classifier_config=classifier_config,
+                tenant_id=tenant_id,
+                iteration_key=iteration_key,
+            )
+            logger.debug(f"Mapping Results:\n{mapping_results}")
 
-        return output_message
+            mapped_materials = add_parsed_data_to_mappings(
+                mapping_results=mapping_results,
+                # TODO: pass materials data from UTD
+                parsed_materials_data=[],
+            )
+
+            output_message = UTDCardOutputMessage(
+                **body.dict(exclude={"guid"}),
+                idn_card_guid=body.guid,
+                guid=iteration_key,
+                status=UTDCardStatus.DONE,
+                # Mapping Data
+                materials=mapped_materials,
+                # Parsed Data
+                supplier_inn=utd_entity.supplier_inn,
+                idn_number=utd_entity.idn_number,
+                idn_date=utd_entity.idn_date,
+                # TODO: set parsed params from UTD pdf file
+                # ! Now it's mocked data
+                organization_inn="3305061878",
+                correction_idn_number="НО-12865РД/2",
+                correction_idn_date=date(2024, 8, 27),
+                contract_name="ДОГОВОР ПОСТАВКИ № 003/06-Лето от 13.09.2023",
+                contract_number="003/06-Лето",
+                contract_date=date(2024, 8, 27),
+            )
+
+            yield output_message
 
     # Handle our errors
     except HTTPException as e:
         error_str = e.detail
-        return raise_utd_card_processing_exception(
+        yield raise_utd_card_processing_exception(
             body=body,
             error_message=error_str,
         )
@@ -97,7 +108,7 @@ def parse_and_map_utd_card(body: UTDCardInputMessage) -> UTDCardOutputMessage:
     # Handle unknown errors
     except Exception as e:
         error_str = str(e)
-        return raise_utd_card_processing_exception(
+        yield raise_utd_card_processing_exception(
             body=body,
             error_message=error_str,
         )
