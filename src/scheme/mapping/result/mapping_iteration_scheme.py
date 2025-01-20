9dc2722308561d24dc2b5cf84e4ded55d14a27af
Syrnnik <sakh.alexandr@gmail.com>
2025-01-20 19:22:35 +0300

    feat: mapping iteration scheme

---
+++ src/scheme/mapping/result/mapping_iteration_scheme.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlmodel import SQLModel, Field, Relationship
+
+
+class MappingIterationBase(SQLModel):
+    metadatas: dict | None = Field(sa_column=Column(JSONB))
+    created_at: datetime | None = Field(default_factory=datetime.utcnow)
+
+
+class MappingIteration(MappingIterationBase, table=True):
+    __tablename__ = "mapping_iteration"
+
+    id: str | None = Field(default=None, primary_key=True)
+
+    results: list["MappingResult"] = Relationship(back_populates="iteration")
+
+
+from scheme.mapping.result.mapping_result_scheme import MappingResult
+
+MappingIteration.update_forward_refs()
