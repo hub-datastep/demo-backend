8d843ef3526d01e8e37fe1b6a01887b8838fe1c7
Syrnnik <sakh.alexandr@gmail.com>
2025-02-04 01:01:39 +0300

    fix: try to fix mapping iteration saving & updating

--- src/model/mapping/result/mapping_iteration_model.py
+++ src/model/mapping/result/mapping_iteration_model.py
@@ -18,3 +18,27 @@ def get_iteration_by_id(
         )
 
     return iteration
+
+
+def create_or_update_iteration(
+    iteration: MappingIteration,
+) -> MappingIteration:
+    iteration: MappingIteration | None = None
+
+    try:
+        # Try to get iteration by id
+        db_iteration = get_iteration_by_id(iteration_id=iteration.id)
+
+        # Update iteration
+        db_iteration.metadatas = iteration.metadatas
+        db_iteration.type = iteration.type
+        iteration = mapping_iteration_repository.update_iteration(
+            iteration=db_iteration,
+        )
+    except HTTPException:
+        # Create iteration if not exists to not lose results
+        iteration = mapping_iteration_repository.create_iteration(
+            iteration=iteration,
+        )
+
+    return iteration
