2279a2e05caa50fed0c25861f547f6a759996e3e
Syrnnik <sakh.alexandr@gmail.com>
2025-02-12 20:45:21 +0300

    refactor: format rules list func

--- src/util/order_classification_rules.py
+++ src/util/order_classification_rules.py
@@ -3,7 +3,5 @@ def format_rules_list(rules: list[str]) -> str:
     Собираем список правил в одну строку,
     где правила записаны в виде ненумерованного списка
     """
-    formatted_rules = "\n".join([
-        f"- {rule}" for rule in rules
-    ])
+    formatted_rules = "\n".join([f"- {rule}" for rule in rules])
     return formatted_rules
