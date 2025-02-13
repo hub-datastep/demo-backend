1682bc21ea8e0b42fe836e0d10ab3aa03bc83c3e
Syrnnik <sakh.alexandr@gmail.com>
2025-02-13 16:15:22 +0300

    fix: remove unused func

--- experiments/order_classification/v6/main.py
+++ experiments/order_classification/v6/main.py
@@ -46,52 +46,6 @@ summary_chain = LLMChain(
     )
 )
 
-
-# TODO: fix or remove this
-def get_order_class(
-    order_query: str,
-    rules_by_classes: dict,
-) -> dict:
-    llm = get_llm_by_client_credentials(client=client)
-
-    query_summary = get_order_query_summary(
-        llm=llm,
-        order_query=order_query,
-        client=client,
-        verbose=verbose,
-    )
-
-    scores = _get_scores_of_classes(
-        llm=llm,
-        order_query=query_summary,
-        rules_by_classes=rules_by_classes,
-        client=client,
-        verbose=verbose,
-    )
-
-    # Class name already exists in score
-    scores_str = "\n\n".join(
-        [f"{score_with_class_name}"
-         for _, score_with_class_name in scores.items()]
-    )
-    # print(scores_str)
-
-    most_relevant_class_response = get_most_relevant_class(
-        llm=llm,
-        order_query=query_summary,
-        rules_by_classes=rules_by_classes,
-        scores=scores_str,
-        client=client,
-        verbose=verbose,
-    )
-
-    return OrderClassificationLLMResponse(
-        most_relevant_class_response=most_relevant_class_response,
-        scores=scores_str,
-        query_summary=query_summary,
-    )
-
-
 if __name__ == "__main__":
     # Шаг 1: Загрузка тест-кейсов
     test_cases = [
