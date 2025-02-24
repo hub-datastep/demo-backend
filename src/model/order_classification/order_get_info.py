aaed52cd08973a41c0adf2f998c211fe3feb9450
Lanutrix <dmodv@vk.com>
2025-02-24 21:38:58 +0300

    logs to file

--- src/model/order_classification/order_get_info.py
+++ src/model/order_classification/order_get_info.py
@@ -1,8 +1,19 @@
 from loguru import logger
 from fastapi.encoders import jsonable_encoder
+from datetime import datetime
+
+# Получаем текущую дату и время для имени файла
+current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_filename = f"order_logs_{current_time}.log"
+
+# Настроим логгер с кастомным форматом
+order_logger = logger.bind(function="get_order_details")
+order_logger.add(log_filename, 
+                 level="INFO",        # Уровень логирования
+                 format="{time:YYYY-MM-DD HH:mm:ss} | {message}")  # Убираем лишнюю информацию
 
 def get_order_details(r, url):
     body_json = jsonable_encoder(r)
-    logger.info(f"{url}: {body_json}")
-
+    # Логи из этой функции будут идти только в файл с датой и временем
+    order_logger.info(f"WEBHOOK | order_updated: {body_json}")
     return
