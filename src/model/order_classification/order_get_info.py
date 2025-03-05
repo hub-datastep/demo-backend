9312bb54c2f1155feffd8bf1401698ba00fc528f
Syrnnik <sakh.alexandr@gmail.com>
2025-03-05 15:12:03 +0300

    fix: error handling

--- src/model/order_classification/order_get_info.py
+++ src/model/order_classification/order_get_info.py
@@ -19,26 +19,31 @@ def get_order_details(body: dict, url: str) -> None:
     )
     logger.debug(log_entry)
 
-    now_date = now.date()
-    log_file_path = f"{env.DATA_FOLDER_PATH}/orders-logs/orders-logs-{now_date}.json"
-
-    # Add log to file
-    if Path(log_file_path).exists():
-        # Save log
-        with open(log_file_path, "r") as log_file:
-            logs = json.load(log_file)
-        with open(log_file_path, "w") as log_file:
-            logs.append(log_entry)
-            json.dump(logs, log_file, ensure_ascii=False)
-
-    # Create full path & log file & add log to it
-    else:
-        # Create logs folder
-        Path(log_file_path).parent.mkdir(
-            parents=True,
-            exist_ok=True,
+    try:
+        now_date = now.date()
+        log_file_path = (
+            f"{env.DATA_FOLDER_PATH}/orders-logs/orders-logs-{now_date}.json"
         )
 
-        # Save log
-        with open(log_file_path, "w") as log_file:
-            json.dump([log_entry], log_file, ensure_ascii=False)
+        # Add log to file
+        if Path(log_file_path).exists():
+            # Save log
+            with open(log_file_path, "r") as log_file:
+                logs = json.load(log_file)
+            with open(log_file_path, "w") as log_file:
+                logs.append(log_entry)
+                json.dump(logs, log_file, ensure_ascii=False)
+
+        # Create full path & log file & add log to it
+        else:
+            # Create logs folder
+            Path(log_file_path).parent.mkdir(
+                parents=True,
+                exist_ok=True,
+            )
+
+            # Save log
+            with open(log_file_path, "w") as log_file:
+                json.dump([log_entry], log_file, ensure_ascii=False)
+    except Exception:
+        logger.error("Cannot save this log to JSON")
