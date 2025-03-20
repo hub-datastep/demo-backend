1a7c0ad960bfc662a9ee28d686ed91bc26f4c6a6
Syrnnik <sakh.alexandr@gmail.com>
2025-03-20 13:35:35 +0300

    feat: action logs names enum

---
+++ src/infra/order_notification.py
@@ -0,0 +1,4 @@
+class ActionLogName:
+    VALIDATE_EVENT = "validate_event"
+    SEND_MESSAGE_TO_RESIDENT = "send_message_to_resident"
+    UPDATE_ORDER_STATUS = "update_order_status"
--- src/model/order_notification/order_notification_model.py
+++ src/model/order_notification/order_notification_model.py
@@ -20,6 +20,7 @@ from infra.domyland.orders import (
     update_order_status_details,
 )
 from infra.llm_clients_credentials import Service, get_llm_by_client_credentials
+from infra.order_notification import ActionLogName
 from llm.chain.cleaning_result_comment.cleaning_result_message_chain import (
     get_cleaning_results_message,
     get_cleaning_results_message_chain,
@@ -183,7 +184,7 @@ def process_event(
         )
         log_record.actions_logs.append(
             {
-                "action": "validate_event",
+                "action": ActionLogName.VALIDATE_EVENT,
                 "metadata": {
                     "is_order_in_pending_status": is_order_in_pending_status,
                     "is_query_exists": is_query_exists,
@@ -314,7 +315,7 @@ def process_event(
                 # )
                 log_record.actions_logs.append(
                     {
-                        "action": "send_message_to_resident",
+                        "action": ActionLogName.SEND_MESSAGE_TO_RESIDENT,
                         "metadata": {
                             "message_text": message_text,
                             "message_files": serialize_objs_list(files_to_send),
@@ -330,7 +331,7 @@ def process_event(
                 )
                 log_record.actions_logs.append(
                     {
-                        "action": "send_message_to_resident",
+                        "action": ActionLogName.SEND_MESSAGE_TO_RESIDENT,
                         "metadata": {
                             "message_text": message_text,
                             "request_body": action_comment,
@@ -341,7 +342,7 @@ def process_event(
         else:
             log_record.actions_logs.append(
                 {
-                    "action": "send_message_to_resident",
+                    "action": ActionLogName.SEND_MESSAGE_TO_RESIDENT,
                     "metadata": {
                         "message_text": message_text,
                         "request_body": skipped_action_text,
@@ -368,7 +369,7 @@ def process_event(
             )
             log_record.actions_logs.append(
                 {
-                    "action": "update_order_status",
+                    "action": ActionLogName.UPDATE_ORDER_STATUS,
                     "metadata": {
                         "request_body": update_order_status_request_body,
                         "response": update_order_status_response,
@@ -378,7 +379,7 @@ def process_event(
         else:
             log_record.actions_logs.append(
                 {
-                    "action": "update_order_status",
+                    "action": ActionLogName.UPDATE_ORDER_STATUS,
                     "metadata": {
                         "request_body": skipped_action_text,
                         "response": skipped_action_text,
