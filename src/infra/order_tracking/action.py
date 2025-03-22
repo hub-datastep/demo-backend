ee964f50ada42371f8dba4b9df6143ae06b16127
Syrnnik <sakh.alexandr@gmail.com>
2025-03-22 12:50:35 +0300

    feat: order tracking task some statuses & actions

---
+++ src/infra/order_tracking/action.py
@@ -0,0 +1,8 @@
+class OrderTrackingTaskAction:
+    # Order API Actions
+    FETCH_ORDER_DETAILS = "Fetch Order Details"
+    UPDATE_ORDER_STATUS = "Update Order Status"
+
+    # Send Message Actions
+    SEND_NEW_ORDER_MESSAGE = "Send SLA Ping-Message"
+    SEND_SLA_PING_MESSAGE = "Send SLA Ping-Message"
