822f197b92297740ddd208648bc918d6a3ce8101
Syrnnik <sakh.alexandr@gmail.com>
2025-03-26 14:51:33 +0300

    fix: send SLA ping message pipeline

--- src/model/order_tracking/actions/sla_ping_message.py
+++ src/model/order_tracking/actions/sla_ping_message.py
@@ -6,39 +6,18 @@ from loguru import logger
 from infra.domyland.chats import get_message_template
 from infra.domyland.constants import MessageTemplateName
 from infra.domyland.orders import get_crm_order_url
-from infra.env import env
-from infra.kafka.brokers import kafka_broker
-from infra.kafka.helpers import send_message_to_kafka
-from middleware.kafka_middleware import with_kafka_broker_connection
+from kafka.helpers.telegram_message_helper import request_to_send_telegram_message
 from scheme.order_classification.order_classification_config_scheme import (
     MessageTemplate,
     ResponsibleUser,
 )
-from scheme.order_notification.order_sla_ping_message_scheme import OrderSLAPingMessage
 
 
-@with_kafka_broker_connection(kafka_broker)
-async def request_to_send_ping_message(
-    order_id: int,
-    message_text: str,
-    chat_id: str,
-):
-    body = OrderSLAPingMessage(
-        message_text=message_text,
-        chat_id=chat_id,
-    )
-    await send_message_to_kafka(
-        broker=kafka_broker,
-        message_body=body,
-        topic=env.KAFKA_ORDER_SLA_PING_MESSAGE_TOPIC,
-        key=str(order_id),
-    )
-
-
-def start_order_sla_tracking(
+def send_sla_ping_message(
     order_id: int,
     responsible_users_list: list[ResponsibleUser],
     messages_templates: list[MessageTemplate],
+    sla_time_left_in_sec: int,
 ):
     # * Get template for SLA ping-message
     template_name = MessageTemplateName.SLA_PING
@@ -50,8 +29,6 @@ def start_order_sla_tracking(
 
     # * Combine CRM Order URL
     crm_order_url = get_crm_order_url(order_id=order_id)
-
-    # TODO: Schedule pinging Responsible Users
 
     # * Send ping-messages to Responsible Users
     for user in responsible_users_list:
@@ -83,6 +60,7 @@ def start_order_sla_tracking(
         formatted_message_text = message_text.format(
             telegram_username=telegram_username,
             crm_order_url=crm_order_url,
+            sla_time_left=f"{sla_time_left_in_sec} секунд",
         )
 
         logger.debug(
@@ -95,7 +73,7 @@ def start_order_sla_tracking(
 
         # Send request to ping Responsible User
         asyncio.run(
-            request_to_send_ping_message(
+            request_to_send_telegram_message(
                 order_id=order_id,
                 message_text=formatted_message_text,
                 chat_id=telegram_chat_id,
