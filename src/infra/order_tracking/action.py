5b5bfc80c876ac0e09e21ca72fa5e643892ad844
Syrnnik <sakh.alexandr@gmail.com>
2025-03-26 19:07:54 +0300

    refactor: orders tasks actions

--- src/infra/order_tracking/action.py
+++ src/infra/order_tracking/action.py
@@ -1,7 +1,6 @@
 class OrderTrackingTaskAction:
     # Order API Actions
     FETCH_ORDER_DETAILS = "Fetch Order Details"
-    UPDATE_ORDER_STATUS = "Update Order Status"
 
     # Send Message Actions
     SEND_NEW_ORDER_MESSAGE = "Send New Order Message"
